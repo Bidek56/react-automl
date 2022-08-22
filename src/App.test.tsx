ad9b135c7ca834630d85acfdb070c31ab55fa6f7
Darek <darek.bidek@gmail.com>
2022-08-22 19:18:51 -0400

    Adding login comp test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { render, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
+import Login from './components/Login';
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.post('/login', (req, res, ctx) => {
+    return res(ctx.json({access_token: 'hello there'}))
+  }),
+)
+
+beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('render App comp', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Copyright/i);
   expect(linkElement).toBeInTheDocument();
+  expect(getByText('AutoML sign in')).toBeInTheDocument();
+});
+
+
+test('render Login comp', async () => {
+
+  const setToken = jest.fn();
+  const { getByText, getByTestId } = render(<Login setToken={setToken} />);
+
+  const linkElement = getByText(/Copyright/i);
+  expect(linkElement).toBeInTheDocument();
+
+  const user = getByTestId("userInput");
+  const pass = getByTestId("passwordInput")
+  const signButton = getByTestId("signButton")
+
+  expect(user).toBeInTheDocument()
+  expect(pass).toBeInTheDocument()
+  expect(signButton).toBeInTheDocument()
+
+  fireEvent.change(user, {
+    target: { value: "admin" },
+  });
+
+  fireEvent.change(pass, {
+    target: { value: "admin" },
+  });
+
+  fireEvent.click(signButton);
 });
